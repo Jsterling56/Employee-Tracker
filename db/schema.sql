e2549b87dd58ab1664d075b1dd99c0aab42f3c50
JSterling56 <jtsterlin@yahoo.com>
2023-09-07 13:28:33 -0400

    restarted. rebuilt tables, server file. readied space for functionality

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,29 @@
-DROP DATABASE IF EXISTS employee_info_db;
+DROP DATABASE IF EXISTS employee_tracker;
 
-CREATE DATABASE employee_info_db;
+CREATE DATABASE employee_tracker;
 
-USE employee_info_db;
+USE employee_tracker;
+
+CREATE TABLE departments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+CREAT TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
 
